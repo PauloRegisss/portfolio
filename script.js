806c67de268d7d38dc9ab59fca9d46faec1dccc1
Paulo Regis <paulocesarregis@hotmail.com>
2026-05-24 04:26:25 -0300

    add typing effect with javascript

--- script.js
+++ script.js
@@ -3,3 +3,18 @@ window.addEventListener("load", () => {
 
     loader.classList.add("fade-out");
 });
+
+const texto = "Código limpo, APIs eficientes e sistemas confiáveis.";
+const elemento = document.getElementById("digitando");
+
+let i = 0;
+
+function escrever() {
+    if(i < texto.length){
+        elemento.innerHTML += texto.charAt(i);
+        i++;
+        setTimeout(escrever, 60);
+    }
+}
+
+escrever();
